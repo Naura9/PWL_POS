11a2281223957d948e0fdad43318e7dd3c2bf156
naura9 <zadoai.naura@gmail.com>
2024-10-02 07:34:27 +0700

    Jobsheet 06 - Praktikum 1,2,3

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -15,6 +15,8 @@
   <link rel="stylesheet" href="{{ asset('adminlte/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
   <link rel="stylesheet" href="{{ asset('adminlte/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
   <link rel="stylesheet" href="{{ asset('adminlte/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
+  <!-- SweetAlert2 -->
+  <link rel="stylesheet" href="{{ asset('adminlte/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css')}}">
   <!-- Theme style -->
   <link rel="stylesheet" href="{{ asset('adminlte/dist/css/adminlte.min.css') }}">
 
@@ -60,7 +62,7 @@
 <!-- jQuery -->
 <script src="{{ asset('adminlte/plugins/jquery/jquery.min.js') }}"></script>
 <!-- Bootstrap 4 -->
-<script src="{{ asset('adminite/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <!-- DataTables & Plugins -->
 <script src="{{ asset('adminlte/plugins/datatables/jquery.dataTables.min.js') }}"></script>
 <script src="{{ asset('adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
@@ -74,8 +76,16 @@
 <script src="{{ asset('adminlte/plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
 <script src="{{ asset('adminlte/plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
 <script src="{{ asset('adminlte/plugins/datatables-buttons/js/buttons.colvis.min.js') }}"></script>
+
+<!-- jQuery Validation -->
+<script src="{{ asset('adminlte/plugins/jquery-validation/jquery.validate.min.js') }}"></script>
+<script src="{{ asset('adminlte/plugins/jquery-validation/additional-methods.min.js') }}"></script>
+
+<!-- SweetAlert2 -->
+<script src="{{ asset('adminlte/plugins/sweetalert2/sweetalert2.min.js') }}"></script>
+
 <!-- AdminLTE App -->
-<script src="{{ asset('adminite/dist/js/adminlte.min.js') }}"></script>
+<script src="{{ asset('adminlte/dist/js/adminlte.min.js') }}"></script>
 <script>
   // Untuk mengirimkan token Laravel CSRF pada setiap request ajax
   $.ajaxSetup({headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')}});
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -5,6 +5,7 @@
         <h3 class="card-title">{{ $page->title }}</h3> 
         <div class="card-tools"> 
           <a class="btn btn-sm btn-primary mt-1" href="{{ url('user/create') }}">Tambah</a> 
+          <button onclick="modalAction('{{ url('/user/create_ajax') }}')" class="btn btn-sm btn-success mt-1">Tambah Ajax</button>
         </div> 
       </div> 
       <div class="card-body">
@@ -27,9 +28,7 @@
                     <div class="col-3">
                         <select class="form-control" id="level_id" name="level_id" required>
                             <option value="">- Semua -</option>
-                            @foreach($level as $item)
-                                <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
-                            @endforeach
+                            
                         </select>
                         <small class="form-text text-muted">Level Pengguna</small>
                     </div>
@@ -49,7 +48,7 @@
             </thead>
         </table>
     </div>
-    
+    <div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" databackdrop="static" data-keyboard="false" data-width="75%" aria-hidden="true"></div>
 @endsection 
  
 @push('css') 
@@ -57,8 +56,15 @@
  
 @push('js') 
   <script> 
+    function modalAction(url = ''){
+      $('#myModal').load(url,function(){
+        $('#myModal').modal('show');
+      });
+    }
+
+    var dataUser;
     $(document).ready(function() { 
-      var dataUser = $('#table_user').DataTable({ 
+      dataUser = $('#table_user').DataTable({ 
           // serverSide: true, jika ingin menggunakan server side processing 
           serverSide: true,      
           ajax: { 
